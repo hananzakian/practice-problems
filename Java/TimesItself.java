8380295d3025d1f34c0b825ccaa3c41c1f43271b
Hanan <hananzakian@gmail.com>
2024-01-01 17:55:14 +0700

    fix recursive function

--- Java/TimesItself.java
+++ Java/TimesItself.java
@@ -1,46 +1,33 @@
 public class TimesItself {
     public static void main(String[] args) {
-        System.out.println(times(29));
+        System.out.println(times(219));
 
-        System.out.println(timesRec(25));
+        System.out.println(timesRec(219));
+//        System.out.println(timesRec2(10));
     }
 
-    public static int times(int x) {
+    public static int times(int n) {
 
-        if (x / 10 == 0) {
-            return 0;
+        if (n / 10 == 0) {
+            return n;
         }
 
         int cont = 1;
-        while (x > 0) {
-            int a = x % 10;
+        while (n > 0) {
+            int a = n % 10;
             cont *= a;
-            x /= 10;
+            n /= 10;
         }
-        x = cont;
 
         return cont;
     }
 
-    public static int timesRec(int x) {
+    public static int timesRec(int n) {
 
-        if (x == 0) {
-            return 0;
-//        } else if (x % 10 == x) {
-//            return x;
-        } else if (x % 10 != 0) {
-            return (x % 10) * timesRec(x - (x % 10));
+        if (n / 10 == 0) {
+            return n;
         }
+        return (n % 10) * timesRec((n - (n % 10))/10);
 
-        return 0;
-    }
-
-    public static int timesRec2(int x) {
-
-        if (x / 10 == 1) {
-
-        } else if (x / 10 != 1) {}
-
-        return 0;
     }
 }
