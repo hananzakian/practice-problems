c0c866cea171faa90b049596342eb4bd5b1a85a5
Hanan <hananzakian@gmail.com>
2024-01-26 00:28:58 +0700

    delete a folder

--- Python/Data Lemur/MinimumInequity.py
+++
@@ -1,8 +0,0 @@
-# https://datalemur.com/questions/python-salary-inequity
-
-def min_inequity(salaries, n):
-    salaries.sort()
-    result = salaries[n-1] - salaries[0]
-    return result
-
-print(min_inequity([60000, 80000, 120000, 70000], 3))
